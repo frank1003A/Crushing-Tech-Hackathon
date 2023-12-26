06be79f4e1a61a11b31397d0843d741b15e00c12
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2023-12-26 14:16:21 +0100

    update app.css and app.js

--- app.js
+++ app.js
@@ -24,11 +24,45 @@ function app() {
   const notificationMenuTrigger = menus[0].querySelector("button");
   const notificationMenu = menus[0].querySelector(".dropdown-container");
 
+  //
+  const activeMenuClass = "menu-active";
+  const activeWidgetFocus = "menu-focus";
+
   window.addEventListener("load", function () {
     setTimeout(() => {
       loader.style.display = "none";
     }, 2000);
   });
+
+  // helper functions
+  const setFocus = (element) => {
+    if (!element) {
+      return;
+    }
+    element.focus();
+  };
+
+  /**error boundary for DOM elements*/
+  const validateElementPresence = (element) => {
+    if (!element) {
+      console.error(
+        `Error: ${element ? element.className : "Element"} not found.`
+      );
+      return;
+    }
+  };
+
+  const isAriaExpanded = (element) => {
+    if (!element) {
+      return;
+    }
+    return element.attributes["aria-expanded"].value === "true";
+  };
+
+  const setDisplayStyleProperty = (element, value) => {
+    validateElementPresence(element);
+    element.style.display = value;
+  };
 
   // Add "step-active" class to the first step by default
   const firstStep = steps[isActive];
@@ -38,7 +72,8 @@ function app() {
    *  TRIAL CARD
    */
   function closeTrialCallout() {
-    trialCall.style.display = "none";
+    trialCall.classList.add("remove");
+    setFocus(guideBtn);
   }
 
   trialCallBtn.addEventListener("click", function () {
@@ -62,10 +97,7 @@ function app() {
   }
 
   guideBtn.addEventListener("click", function () {
-    if (!guideContainer) {
-      console.error("Error: guide-container element not found.");
-      return;
-    }
+    validateElementPresence(guideContainer);
 
     // Toggle the "display_none" class on the stepper element
     guideContainer.classList.toggle("display_none");
@@ -73,21 +105,17 @@ function app() {
     // Call the function to rotate the chevron icon
     rotateChevronIcon(this);
 
-    const isExpanded =
-      guideContainer.attributes["aria-expanded"].value === "true";
+    const isExpanded = isAriaExpanded(guideContainer);
 
     if (isExpanded) {
-      this.ariaLabel = "collapse guide";
+      this.setAttribute("aria-label", "collapse guide");
     } else {
-      this.ariaLabel = "expand guide";
+      this.setAttribute("aria-label", "expand guide");
     }
   });
 
   function expandStepRegion(step, index) {
-    if (!step) {
-      console.error("Error: Step element not found.");
-      return;
-    }
+    validateElementPresence(step);
 
     // Remove "step-active" class and set aria-expanded to false for all steps
     for (let i = 0; i < steps.length; i++) {
@@ -178,14 +206,17 @@ function app() {
       return;
     }
 
-    let s = Array.from(steps).filter((s, i) => {
+    Array.from(steps).filter((s, i) => {
       if (!completed.includes(i)) {
         incompleteSteps.push(i);
       }
     });
+
     if (incompleteSteps.length > 0) {
-      expandStepRegion(steps[incompleteSteps[0]], incompleteSteps[0]);
-      steps[incompleteSteps[0]].focus();
+      let index = incompleteSteps[0];
+      let nextIncompleteStep = steps[incompleteSteps[0]];
+      expandStepRegion(nextIncompleteStep, index);
+      setFocus(checkBoxButtons[index]);
     } else {
       return;
     }
@@ -200,7 +231,10 @@ function app() {
     steps.forEach((step, index) => {
       // event listener to each step for keyboard interaction.
       step.addEventListener("keyup", function (event) {
-        handleItemsArrowKeyPress(steps, event, index);
+        if (event.key === "Enter" || event.key === " ") {
+          expandStepRegion(steps[index], index);
+          setFocus(checkBoxButtons[index]);
+        }
       });
     });
   });
@@ -215,20 +249,13 @@ function app() {
   const handleItemsArrowKeyPress = (items, event, index) => {
     const isLast = index === items.length - 1;
     const isFirst = index === 0;
-    const next = items.item(index + 1);
-    const prev = items.item(index - 1);
-    const isStep = items[index].className.includes("step");
-    items[0].ariaExpanded = "true";
-
-    if (event.key === "Enter" || event.key === " ") {
-      if (isStep) {
-        expandStepRegion(items[index], index);
-      }
-    }
+    const next = items[index + 1];
+    const prev = items[index - 1];
+    //items[0].setAttribute("aria-expanded", "true");
 
     if (event.key === "ArrowRight" || event.key === "ArrowDown") {
       if (isLast) {
-        items.item(0).focus();
+        items[0].focus();
         return;
       }
       next.focus();
@@ -236,13 +263,55 @@ function app() {
 
     if (event.key === "ArrowUp" || event.key === "ArrowLeft") {
       if (isFirst) {
-        items.item(items.length - 1).focus();
+        items[items.length - 1].focus();
         return;
       }
-
       prev.focus();
     }
+
+    if (event.key === "Home") {
+      items[0].focus();
+    }
+
+    if (event.key === "End") {
+      items[items.length - 1].focus();
+    }
   };
+
+  function getVisibleTabbableElements(element = document) {
+    return Array.from(
+      element.querySelectorAll(
+        'a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])'
+      )
+    ).filter((el) => {
+      const style = window.getComputedStyle(el);
+      const isVisible = style.visibility !== "hidden" && style.opacity !== "0";
+      const hasPositiveDimensions = el.offsetWidth > 0 && el.offsetHeight > 0;
+      return isVisible && hasPositiveDimensions;
+    });
+  }
+
+  document.addEventListener("keyup", function (e) {
+    if (
+      profileMenu.classList.contains(activeMenuClass) ||
+      notificationMenu.classList.contains(activeMenuClass)
+    ) {
+      return;
+    } else {
+      const allFocusableElement = getVisibleTabbableElements();
+
+      const handleTabKey = function (event) {
+        const index = allFocusableElement.indexOf(event.target);
+
+        if (index !== -1) {
+          handleItemsArrowKeyPress(allFocusableElement, event, index);
+        }
+      };
+      allFocusableElement.forEach((el) => {
+        el.addEventListener("keyup", handleTabKey);
+      });
+    }
+  });
 
   /**
    * KEYBOARD INTERACTIONS
@@ -254,55 +323,75 @@ function app() {
    */
 
   // Handling the Escape key press to close associated menus.
-  function handleMenuEscapeKeyPress(event) {
+  function handleMenuEscapeKeyPress(event, menu, button) {
     // Checking if the pressed key is "Escape".
     if (event.key == "Escape") {
       // Closing profile and notification menus.
-      closeMenu(profileMenu, profileMenuTrigger);
-      closeMenu(notificationMenu, notificationMenuTrigger);
+      closeMenu(menu, button);
     }
   }
 
-  // Closing the menu by removing the "menu-active" class and updating aria-expanded.
+  // Closing the menu by removing the active menu class and updating aria-expanded.
   function closeMenu(menu, button) {
     // Checking if the menu is currently active.
-    if (menu.classList.contains("menu-active")) {
-      // Removing the "menu-active" class to hide the menu.
-      menu.classList.remove("menu-active");
-      button.classList.remove("menu-focus");
-
-      // Updating the aria-expanded attribute to indicate the menu is closed.
-      button.ariaExpanded = "false";
-
-      // Returning focus to the button that triggered the menu.
-      button.focus();
+    const isMenuOpen = menu.classList.contains(activeMenuClass);
+    if (!isMenuOpen) {
+      return;
     }
+    // Removing the active menu class to hide the menu.
+    menu.classList.remove(activeMenuClass);
+    button.classList.remove(activeWidgetFocus);
+
+    // Updating the aria-expanded attribute to indicate the menu is closed.
+    button.setAttribute("aria-expanded", "false");
+
+    // Returning focus to the button that triggered the menu.
+    button.focus();
+  }
+
+  function openMenu(menu, button) {
+    // Checking if the menu is currently active.
+    const isMenuOpen = menu.classList.contains(activeMenuClass);
+    if (isMenuOpen) {
+      return;
+    }
+    // Add the activeMenuClass class to show the menu.
+    menu.classList.add(activeMenuClass);
+    button.classList.add(activeWidgetFocus);
+
+    // Updating the aria-expanded attribute to indicate the menu is open.
+    button.setAttribute("aria-expanded", "true");
   }
 
   function toggleMenu(menu, button) {
     const isExpanded = button.attributes["aria-expanded"].value === "true";
-    const allMenuItems = menu.querySelectorAll("a");
-    const allButtons = menu.querySelectorAll("button");
-    menu.classList.toggle("menu-active");
-    button.classList.add("menu-focus");
+    const allMenuItems = menu.querySelectorAll("a, button");
+
+    if (menu.classList.contains(activeMenuClass)) {
+      closeMenu(menu, button);
+    } else {
+      openMenu(menu, button);
+    }
 
     if (isExpanded) {
-      button.ariaExpanded = "false";
+      button.setAttribute("aria-expanded", "false");
       button.focus();
-    } else {
-      button.ariaExpanded = "true";
-      allMenuItems.item(0)
-        ? allMenuItems.item(0).focus()
-        : allButtons.item(0).focus();
-
-      menu.addEventListener("keyup", handleMenuEscapeKeyPress);
-      allMenuItems.forEach((item, index) => {
-        item.addEventListener("keyup", (event) => {
-          // Handling key presses for navigation within a menu.
-          handleItemsArrowKeyPress(allMenuItems, event, index);
-        });
-      });
+      return;
     }
+
+    button.setAttribute("aria-expanded", "true");
+    allMenuItems ? allMenuItems[0].focus() : null;
+
+    menu.addEventListener("keyup", function (event) {
+      handleMenuEscapeKeyPress(event, menu, button);
+    });
+
+    allMenuItems.forEach((item, index) => {
+      item.addEventListener("keyup", (event) => {
+        // Handling key presses for navigation within a menu.
+        handleItemsArrowKeyPress(allMenuItems, event, index);
+      });
+    });
   }
 
   profileMenuTrigger.addEventListener("click", function () {
@@ -314,12 +403,19 @@ function app() {
   });
 
   window.addEventListener("mouseup", function (event) {
-    if (event.target != profileMenu && event.target.parentNode != profileMenu) {
+    if (
+      event.target != profileMenu &&
+      event.target.parentNode != profileMenu &&
+      event.target !== profileMenuTrigger &&
+      event.target.parentNode != profileMenuTrigger
+    ) {
       closeMenu(profileMenu, profileMenuTrigger);
     }
     if (
       event.target != notificationMenu &&
-      event.target.parentNode != notificationMenu
+      event.target.parentNode != notificationMenu &&
+      event.target !== notificationMenuTrigger &&
+      event.target.parentNode != notificationMenuTrigger
     ) {
       closeMenu(notificationMenu, notificationMenuTrigger);
     }
